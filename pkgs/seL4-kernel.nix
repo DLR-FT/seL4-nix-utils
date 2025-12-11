d3f20bbcaa40073038a158caf11e7da81fce7ac5
wucke13 <wucke13+github@gmail.com>
2025-12-11 16:53:38 +0100

    chore: upgrade seL4-kernel to 14.0.0
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

--- pkgs/seL4-kernel.nix
+++ pkgs/seL4-kernel.nix
@@ -17,36 +17,55 @@
 let
   # known seL4 configs
   knownVerifiedConfigs = [
+    "AARCH64_bcm2711_verified"
+    "AARCH64_hikey_verified"
+    "AARCH64_imx8mm_verified"
+    "AARCH64_imx8mq_verified"
+    "AARCH64_imx93_verified"
+    "AARCH64_maaxboard_verified"
+    "AARCH64_odroidc2_verified"
+    "AARCH64_odroidc4_verified"
+    "AARCH64_rockpro64_verified"
+    "AARCH64_tqma_verified"
+    "AARCH64_tx1_verified"
+    "AARCH64_ultra96v2_verified"
     "AARCH64_verified"
+    "AARCH64_zynqmp_verified"
+    "ARM_HYP_exynos5410_verified"
     "ARM_HYP_exynos5_verified"
     "ARM_HYP_verified"
     "ARM_MCS_verified"
+    "ARM_am335x_verified"
+    "ARM_bcm2837_verified"
+    "ARM_exynos4_verified"
+    "ARM_exynos5410_verified"
+    "ARM_exynos5422_verified"
+    "ARM_hikey_verified"
     "ARM_imx8mm_verified"
+    "ARM_omap3_verified"
+    "ARM_tk1_verified"
     "ARM_verified"
+    "ARM_zynq7000_verified"
+    "ARM_zynqmp_verified"
     "RISCV64_MCS_verified"
     "RISCV64_verified"
     "X64_verified"
+    "seL4Config"
   ]
   ++ extraVerifiedConfigs;
-
-  brokenVerifiedConfigs = [
-    "ARM_HYP_exynos5_verified"
-    "ARM_HYP_verified"
-  ];
-
 in
 # check that the passed seL4 config is known
 assert if verifiedConfig != null then builtins.elem verifiedConfig knownVerifiedConfigs else true;
 
 stdenv.mkDerivation rec {
   pname = "seL4";
-  version = "13.0.0";
+  version = "14.0.0";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = version;
-    hash = "sha256-sFmOFoXwe1AsG0JLI2+QKEitmecxik9lxXUqf4QRysk=";
+    hash = "sha256-kzRV3qIsfyIFoc2hT6l0cIyR6zLD4yHcPXCAbGAQGsk=";
   };
 
   nativeBuildInputs = [
@@ -70,9 +89,6 @@ stdenv.mkDerivation rec {
   ]
   ++ lib.lists.optional (verifiedConfig != null) "-C../configs/${verifiedConfig}.cmake"
   ++ extraCmakeFlags;
-
-  # TODO remove hotfix for https://github.com/seL4/seL4/issues/1334
-  patches = lib.lists.optional (lib.lists.elem verifiedConfig brokenVerifiedConfigs) ../patches/seL4-cmake-install-target-failure.patch;
 
   dontFixup = true;
 }
