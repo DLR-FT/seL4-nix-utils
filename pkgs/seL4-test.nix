2c4275732c79a5b35b4ced2033982bafceca869a
wucke13 <wucke13+github@gmail.com>
2026-08-03 12:24:44 +0200

    chore: upgrade seL4-test 14.0.0 -> 15.0.0
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

--- pkgs/seL4-test.nix
+++ pkgs/seL4-test.nix
@@ -17,12 +17,12 @@
 
 stdenv.mkDerivation rec {
   pname = "seL4test";
-  version = "14.0.0";
+  version = "15.0.0";
 
   src = fetchGoogleRepoTool {
     url = "https://github.com/seL4/sel4test-manifest.git";
     rev = version;
-    hash = "sha256-RU48CUGA52fbHXOjpKrJIeMTcuOhVtgsRqsaFsxQowc=";
+    hash = "sha256-91IJh50dUahNsvGuA8h2Tq99PNZLboHX/eJmE92Ccw8=";
   };
 
   nativeBuildInputs = [
